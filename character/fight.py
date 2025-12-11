be1a2e825755ccc62eb1e7bf958e2c2e248cd869
nngghh <107542316+nngghh@users.noreply.github.com>
2025-12-11 13:10:00 +0900

    Update fight.py

--- character/fight.py
+++ character/fight.py
@@ -75,13 +75,14 @@ class Fight(character.Character):
                 time.sleep(1.5)
                 print('\n라이프 1 감소')
                 time.sleep(1.5)
-                self.hero.life -= 1
+                self.hero.life_down()
                 print('남은 라이프 {}'.format(self.hero.life))
                 # self.hero.hp =100 * character.Character.levelhp
                 break
 
 
             turn += 1
+
 
 
 
