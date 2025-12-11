1bd3504d901c8102b35f64abfbb0a24535ad4ee4
nngghh <107542316+nngghh@users.noreply.github.com>
2025-12-11 19:24:50 +0900

    Update monster.py

--- character/monster.py
+++ character/monster.py
@@ -11,19 +11,20 @@ class Monster(character.Character):
       self.hp = int((self.hp * scale))
       self.ap = int(self.ap * scale)
       if mon_name == '스톤 골렘':
-        self.hp=self.hp * 2
+        self.hp=self.hp * 1.5
       elif mon_name =='오크 전사':
-        self.hp*=2
+        self.hp*=1.5
         self.ap*=1.5
       elif mon_name =='불의 정령':
         self.hp*=0.9
         self.ap*=1.4
       elif mon_name =='드래곤':
         self.ap*=1.575
-        self.hp*=2.15
+        self.hp*=2
   @classmethod
   def make_char(cls,mon_name):
 
     return cls(mon_name)
+
 
 
