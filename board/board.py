4d2105dae3d48b275aa6fcf6b61f6be7dd735078
nngghh <107542316+nngghh@users.noreply.github.com>
2025-12-11 14:01:52 +0900

    Update board.py

--- board/board.py
+++ board/board.py
@@ -16,7 +16,7 @@ class Board:
     [섞은 전투 칸, 아이템 칸, 빈칸에 보스 칸까지 추가]"""
     total = row * col
     event_list = []
-    tile = [battleevent.BattleEvent(),battleevent.BattleEvent(),itemevent.ItemEvent(), itemevent.ItemEvent(), noevent.NoEvent(),minigameevent.MinigameEvent()]
+    tile = [battleevent.BattleEvent(),battleevent.BattleEvent(),battleevent.BattleEvent(),itemevent.ItemEvent(), itemevent.ItemEvent(), noevent.NoEvent(),minigameevent.MinigameEvent()]
     for _ in range((total+1)//len(tile)):
       random.shuffle(tile)
       event_list += tile
@@ -73,4 +73,5 @@ class Board:
         print()
         if r%2: print(' '*14 + '🌲🌳'*5*(self.col-1))
         else:   print('🌲🌳'*5*(self.col-1))
+
         print()
