9d1f5a428bce96a03f08b53373dc02bdcc0082a3
nngghh <107542316+nngghh@users.noreply.github.com>
2025-12-11 13:03:01 +0900

    Update fightcharacter.py

--- character/fightcharacter.py
+++ character/fightcharacter.py
@@ -1,15 +1,15 @@
 from character.character import Character
 from common import play
 class FightCharacter(Character):
-  def __init__(self, roll, char_name,hp,ap,life,used_item,acquired_item):
+  def __init__(self, roll, player, used_item):
     super().__init__(char_name)
-    self.hp = hp
-    self.ap = ap
-    self.char_name = char_name
-    self.life = life
+    self.hp = player.hp
+    self.ap = player.ap
+    self.char_name = player.char_name
+    self.life = player.life
     self.used_item = used_item
     self.roll = roll
-    self.acquired_item = acquired_item
+    self.acquired_item = player.acquired_item
     n = {9:12,8:13,7:14,6:15,5:16,4:17,3:18,2:19,1:20,0:20}
     if self.used_item == '특별 20면 주사위' :
       if self.roll >= 11 :
@@ -32,6 +32,7 @@ class FightCharacter(Character):
 
         self.hp += self.roll * 10
         self.ap += self.roll * 2
+
 
 
 
